c3e3f2d1d3dcf90ee776cbc93a9e49b2600f6a4a
vgaidarji <veaceslav.gaidarji@gmail.com>
2020-05-20 22:58:43 +0300

    Print GitHub payload to console

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+
 const ActionConfig = require('./config/action-config');
 
 try {
+  console.log(`payload: ${JSON.stringify(github.context.payload)}`);
+
+  if (github.event_name === 'issue_comment' &&
+      github.event.action == 'created') {
+    console.log(`comment: ${github.event.comment.body}`);
+  }
+
   const actionConfig = new ActionConfig(core);
   if (actionConfig.isDryRun) {
     console.log('is dry run = true');
