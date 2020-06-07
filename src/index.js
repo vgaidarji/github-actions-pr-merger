daaeb320c6ff78553a547d11f4353b29cf615366
vgaidarji <veaceslav.gaidarji@gmail.com>
2020-06-07 13:01:18 +0300

    Adjust async/await create comment command

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const ActionConfig = require('./config/action-config');
 const octokit = new Octokit();
 
 try {
-  const githubPayload = github.context.payload
+  const githubPayload = github.context.payload;
   console.log(`payload: ${JSON.stringify(github.context.payload)}`);
 
   if (github.event_name === 'issue_comment' &&
@@ -16,16 +16,17 @@ try {
   const actionConfig = new ActionConfig(core);
   if (actionConfig.isDryRun) {
     console.log(`is dry run = ${actionConfig.isDryRun}`);
-    const { owner, repo, number } = githubPayload.issue
-    const { data: comment } = await octokit.issues.createComment({
-      owner: owner,
-      repo: repo,
-      issue_number: number,
-      body: "dry-run test merge commit message",
-    });
-    core.info(
-      `Created comment id '${comment.id}' on issue '${githubPayload.issue.id}'.`
-    );
+    const {owner, repo, number} = githubPayload.issue;
+    const comment = async () => {
+      const {data: comment} = await octokit.issues.createComment({
+        owner: owner,
+        repo: repo,
+        issue_number: number,
+        body: 'dry-run test merge commit message',
+      });
+      return comment;
+    };
+    core.info(`Created comment id '${comment.id}' on issue '${githubPayload.issue.id}'.`);
   }
 } catch (error) {
   core.setFailed(error.message);
