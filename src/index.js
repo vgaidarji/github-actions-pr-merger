0e5f893d608e18cb65831531d2328af8c646f772
vgaidarji <veaceslav.gaidarji@gmail.com>
2020-06-07 12:43:16 +0300

    Initialize octokit variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
 const ActionConfig = require('./config/action-config');
+
+const octokit = new Octokit();
 
 try {
   const githubPayload = github.context.payload
