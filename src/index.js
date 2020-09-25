d354f2a1227f4c121a66187934848dbd95033db4
vgaidarji <veaceslav.gaidarji@gmail.com>
2020-09-25 12:45:49 +0300

    Introduce PullRequest wrapper class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const ActionConfig = require('./config/action-config');
+const PullRequest = require('./github/pull-request');
 
 const {GITHUB_TOKEN} = process.env;
 const {context: githubContext} = github;
@@ -8,6 +9,7 @@ const {context: githubContext} = github;
 const octokit = github.getOctokit(GITHUB_TOKEN);
 
 try {
+  const pullRequest = new PullRequest(githubContext.payload);
   console.log(`payload: ${JSON.stringify(githubContext.payload)}`);
 
   if (github.event_name === 'issue_comment' &&
@@ -18,17 +20,14 @@ try {
   const actionConfig = new ActionConfig(core);
   if (actionConfig.isDryRun) {
     console.log(`is dry run = ${actionConfig.isDryRun}`);
-    const owner = githubContext.payload.repository.owner.login;
-    const repo = githubContext.payload.repository.name;
-    const number = githubContext.payload.pull_request.number;
     const postComment = async () => {
       const {data: comment} = await octokit.issues.createComment({
-        owner: owner,
-        repo: repo,
-        issue_number: number,
+        owner: pullRequest.owner,
+        repo: pullRequest.repo,
+        issue_number: pullRequest.number,
         body: 'dry-run test merge commit message',
       });
-      console.log(`Created comment '${comment.body}' on issue '${number}'.`);
+      console.log(`Created comment '${comment.body}' on issue '${pullRequest.number}'.`);
       return comment;
     };
     postComment();
