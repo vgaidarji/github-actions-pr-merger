d134e9219dd5b1f6050bbfdcdfc4a961deb0c000
Veaceslav Gaidarji <vgaidarji@users.noreply.github.com>
2020-09-25 15:11:37 +0300

    Check for PR before posting comment in dry-run mode (#14)

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ try {
   console.log(`payload: ${JSON.stringify(githubContext.payload)}`);
 
   const isComment = 'comment' in githubContext.payload;
-  // const isPullRequest = 'pull_request' in githubContext.payload;
+  const isPullRequest = 'pull_request' in githubContext.payload;
 
   if (isComment && githubContext.payload.action == 'created') {
     console.log(`comment: ${githubContext.payload.comment.body}`);
@@ -22,17 +22,21 @@ try {
   const actionConfig = new ActionConfig(core);
   if (actionConfig.isDryRun) {
     console.log(`is dry run = ${actionConfig.isDryRun}`);
-    const postComment = async () => {
-      const {data: comment} = await octokit.issues.createComment({
-        owner: pullRequest.owner,
-        repo: pullRequest.repo,
-        issue_number: pullRequest.number,
-        body: 'dry-run test merge commit message',
-      });
-      console.log(`Created comment '${comment.body}' on issue '${pullRequest.number}'.`);
-      return comment;
-    };
-    postComment();
+    if (isPullRequest) {
+      const postComment = async () => {
+        const {data: comment} = await octokit.issues.createComment({
+          owner: pullRequest.owner,
+          repo: pullRequest.repo,
+          issue_number: pullRequest.number,
+          body: 'dry-run test merge commit message',
+        });
+        console.log(`Created comment '${comment.body}' on issue '${pullRequest.number}'.`);
+        return comment;
+      };
+      postComment();
+    } else if (isComment) {
+      // pull request properties (owner, repo, number) should be extracted differently
+    }
   }
 } catch (error) {
   core.setFailed(error.message);
