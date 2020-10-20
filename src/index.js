b7290b3168556214a515d9aec9845006cc4ab4cd
Veaceslav Gaidarji <vgaidarji@users.noreply.github.com>
2020-10-20 18:52:40 +0300

    Dont trigger dry run on PRs automatically (#21)
    Eradicated dry-run action configuration. Triggering action in dry-run mode for PR builds makes not much sense since main action purpose is to be triggered via PR comments. Having no comment object present in GitHub payload in action workflow build makes it impossible to perform full integration test on each PR. Instead, to test the action comment with dry-run parameter should be left on PR.
    
    See #20 for more context.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const ActionConfig = require('./config/action-config');
 const PullRequest = require('./github/pull-request');
 
 const {GITHUB_TOKEN} = process.env;
@@ -9,6 +8,7 @@ const {context: githubContext} = github;
 const octokit = github.getOctokit(GITHUB_TOKEN);
 
 const ROBIN_COMMAND = '/Robin';
+const HAS_DRY_RUN_FLAG = '--dry-run';
 
 // on PR
 // on comment with command `/Robin squash-merge` or `/Robin rebase-merge`
@@ -26,38 +26,40 @@ try {
   console.log(`payload: ${JSON.stringify(githubContext.payload)}`);
 
   const isComment = 'comment' in githubContext.payload;
+  // TODO: extract pull_request from 'issue' property from comment payload
   const isPullRequest = 'pull_request' in githubContext.payload;
+  const isCommentCreatedAction = isComment && githubContext.payload.action == 'created';
 
-  if (isComment && githubContext.payload.action == 'created') {
-    console.log(`comment: ${githubContext.payload.comment.body}`);
-    if (githubContext.payload.comment.body.includes(ROBIN_COMMAND)) {
+  if (isCommentCreatedAction) {
+    const commentBody = githubContext.payload.comment.body;
+    // TODO: extract into a func with proper format check and not only for robin keyword
+    const isTriggeredViaRobinCommand = commentBody.includes(ROBIN_COMMAND);
+    const isDryRunMode = commentBody.includes(HAS_DRY_RUN_FLAG);
+    console.log(`comment: ${commentBody}`);
+
+    if (isTriggeredViaRobinCommand) {
       console.log(`Triggered via ${ROBIN_COMMAND} command.`);
+      console.log(`is dry run = ${isDryRunMode}`);
+
+      if (isPullRequest && isDryRunMode) {
+        const pullRequest = new PullRequest(githubContext.payload);
+        const postComment = async () => {
+          const {data: comment} = await octokit.issues.createComment({
+            owner: pullRequest.owner,
+            repo: pullRequest.repo,
+            issue_number: pullRequest.number,
+            body: 'dry-run test merge commit message',
+          });
+          console.log(`Created comment '${comment.body}' on issue '${pullRequest.number}'.`);
+          return comment;
+        };
+        postComment();
+      }
     } else {
       console.log(
           `Robin helps only when he has been explicitly asked via \`/Robin\` command.
           See https://github.com/vgaidarji/github-actions-pr-merger/tree/master#usage.`);
       return;
-    }
-  }
-
-  const actionConfig = new ActionConfig(core);
-  if (actionConfig.isDryRun) {
-    console.log(`is dry run = ${actionConfig.isDryRun}`);
-    if (isPullRequest) {
-      const pullRequest = new PullRequest(githubContext.payload);
-      const postComment = async () => {
-        const {data: comment} = await octokit.issues.createComment({
-          owner: pullRequest.owner,
-          repo: pullRequest.repo,
-          issue_number: pullRequest.number,
-          body: 'dry-run test merge commit message',
-        });
-        console.log(`Created comment '${comment.body}' on issue '${pullRequest.number}'.`);
-        return comment;
-      };
-      postComment();
-    } else if (isComment) {
-      // pull request properties (owner, repo, number) should be extracted differently
     }
   }
 } catch (error) {
