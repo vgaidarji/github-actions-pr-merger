11a23e9d63f3da9ee4b577208ea949ece685efce
vgaidarji <veaceslav.gaidarji@gmail.com>
2021-07-26 18:53:26 +0300

    Print PR for testing purposes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const MergeMethod = require('./github/merge-method');
+// const MergeMethod = require('./github/merge-method');
 const RobinCommand = require('./robin/robin-command');
 
 const {GITHUB_TOKEN} = process.env;
